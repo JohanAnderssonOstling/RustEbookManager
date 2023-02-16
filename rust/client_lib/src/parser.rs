6df0828cbd4992338ccf01bd85c1438182d43b95
johandost <johan.andersson.ostling@protonmail.com>
2023-02-16 20:00:48 +0100

    Fix

--- rust/client_lib/src/parser.rs
+++ rust/client_lib/src/parser.rs
@@ -1,4 +1,5 @@
-
+use std::fs;
+use std::path::PathBuf;
 use uuid::Uuid;
 use crate::library_model::ffi::Book;
 
@@ -9,6 +10,8 @@ pub fn parse_book(path: &str, thumb_dir: PathBuf) -> Book{
     let uuid = Uuid::new_v4().to_string();
     let file_name = path.split("/").last().unwrap().to_string();
     let file_extension = file_name.split(".").last().unwrap().to_string();
+    let mut book_thumb_dir = thumb_dir.join(uuid.clone());
+    fs::create_dir_all(&book_thumb_dir).unwrap();
     let mut book =  Book{
         uuid,
         name: file_name,
@@ -16,8 +19,8 @@ pub fn parse_book(path: &str, thumb_dir: PathBuf) -> Book{
         ..Default::default()
     };
     match file_extension.as_str() {
-        "epub" => epubparser::parse_epub(book, thumb_dir),
-        "pdf" => pdfparser::parse_pdf(book, thumb_dir),
+        "epub" => epubparser::parse_epub(book, book_thumb_dir),
+        "pdf" => pdfparser::parse_pdf(book, book_thumb_dir),
         _ => book,
     }
 }
