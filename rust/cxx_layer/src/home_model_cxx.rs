42a65b6f00a7629652b11403064d3535a524a497
johan <johan.andersson.ostling@protonmail.com>
2023-03-07 00:08:19 +0100

    Finish moving C++ layer to new crate

--- rust/cxx_layer/src/home_model_cxx.rs
+++ rust/cxx_layer/src/home_model_cxx.rs
@@ -1,17 +1,13 @@
 use crate::home_model_cxx::ffi::*;
-
+use client_lib::home_model;
 pub fn add_library(name: &str, path: &str) -> Library {
-	Library {
-		name: String::from(name),
-		uuid: String::from(""),
-		path: String::from(""),
-	}
+	unsafe { std::mem::transmute(home_model::add_library(name, path)) }
 }
 pub fn get_library_list() -> Vec<Library> {
-	vec![]
+	unsafe { std::mem::transmute(home_model::get_library_list()) }
 }
 pub fn delete_library(library_uuid: &str) {
-
+	home_model::delete_library(library_uuid);
 }
 #[cxx::bridge]
 mod ffi {
--- rust/cxx_layer/src/library_model_cxx.rs
+++ rust/cxx_layer/src/library_model_cxx.rs
@@ -1,35 +1,64 @@
-use epub::doc::EpubDoc;
+use client_lib::library_model;
+use crate::library_model_cxx::ffi::*;
 
-use crate::library_model_cxx::ffi::{Book, Dir, ReadPosition};
 
 pub fn open_library(uuid: &str, path: &str) {
+	library_model::open_library(uuid, path);
 }
 
 pub fn scan_library(model_uuid: String) {
-
+	library_model::scan_library(model_uuid);
 }
 pub fn get_books(model_uuid: String, folder_id: u32) -> Vec<Book> {
-	vec![]
+	let books = library_model::get_books(model_uuid, folder_id);
+	let mut result = Vec::new();
+	for book in books {
+		let book = Book {
+			uuid: book.uuid,
+			name: book.name,
+			path: book.path,
+			read_location: book.read_location,
+			read_percentage: book.read_percentage,
+			title: book.title,
+			isbn: book.isbn,
+			folder_id: book.folder_id,
+		};
+		result.push(book);
+	}
+	result
+
 }
 pub fn get_folders(model_uuid: String, parent_id: u32) -> Vec<Dir> {
-	vec![]
+	let dirs = library_model::get_folders(model_uuid, parent_id);
+	let mut result = Vec::new();
+	for dir in dirs {
+		let dir = Dir {
+			id: dir.id,
+			name: dir.name,
+			parent_id: dir.parent_id,
+		};
+		result.push(dir);
+	}
+	result
 }
 pub fn get_cover_path(model_uuid: String, book_uuid: &str) -> String {
-	String::from("")
+	library_model::get_cover_path(model_uuid, book_uuid)
 }
 
 pub fn set_book_location(model_uuid: String, book_uuid: &str, location: &str, percentage: u32) {
+	library_model::set_book_location(model_uuid, book_uuid, location, percentage);
 }
 
 pub fn get_book_location(model_uuid: String, book_uuid: &str) -> ReadPosition {
+	let read_position = library_model::get_book_location(model_uuid, book_uuid);
 	ReadPosition {
-		read_location: String::from(""),
-		read_percentage: 0,
+		read_location: read_position.read_location,
+		read_percentage: read_position.read_percentage,
 	}
 }
 
 pub fn get_cover_widths() -> Vec<u32> {
-	vec![100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+	library_model::get_cover_widths()
 }
 
 
